7eb0845f1ed2d9e5c52b57c8886d2fba80a485be
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-10-08 12:32:44 -0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+      },
+      colors: {
+        primary: {
+          300: '#F1E9C9',
+          500: '#DBAC2C',
+          700: '#C47F17',
+        },
+
+        secondary: {
+          300: '#F1E9C9',
+          500: '#8047F8',
+          700: '#4B2995',
+        },
+
+        base: {
+          100: '#FAFAFA',
+          200: '#F3F2F2',
+          300: '#EDEDED',
+          400: '#E6E5E5',
+          500: '#D7D5D5',
+          600: '#8D8686',
+          700: '#574F4D',
+          800: '#403937',
+          900: '#272221',
+        },
       },
     },
   },
