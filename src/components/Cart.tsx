418d92c8ea56b27e0273c90b02dfa54676e2b3a6
Jeffersdon Silva <jeffsilva01.dev@gmail.com>
2023-10-24 22:57:12 -0300

    added count in cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,18 @@
 'use client'
+import { useCartContext } from '@/context/CartContext'
 import { ShoppingCart } from 'lucide-react'
 import Link from 'next/link'
 
 export function Cart() {
+  const { coffees } = useCartContext()
+
   return (
-    <Link className="rounded-md bg-primary-300 p-2" href="/cart">
+    <Link
+      data-hidden="teste"
+      data-count={coffees.length}
+      className="relative rounded-md bg-primary-300 p-2 before:absolute before:-right-2 before:-top-2 before:h-5 before:w-5 before:rounded-full before:bg-primary-500 before:text-center before:text-sm before:font-bold  before:text-white before:content-[attr(data-count)] before:data-[count='0']:hidden"
+      href="/cart"
+    >
       <ShoppingCart size={22} className="fill-primary-700 text-primary-700" />
     </Link>
   )
