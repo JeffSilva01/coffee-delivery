0d48c2dc893f541c66c4bd4f6da549bea5747775
Jeffersdon Silva <jeffsilva01.dev@gmail.com>
2023-10-26 20:44:54 -0300

    creat checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+export default function Checkout() {
+  return (
+    <section className="flex h-screen w-screen items-center justify-center">
+      <div>
+        <div>
+          <h2>Complete seu pedido</h2>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,7 +11,7 @@ export function Cart() {
       data-hidden="teste"
       data-count={coffees.length}
       className="relative rounded-md bg-primary-300 p-2 before:absolute before:-right-2 before:-top-2 before:h-5 before:w-5 before:rounded-full before:bg-primary-500 before:text-center before:text-sm before:font-bold  before:text-white before:content-[attr(data-count)] before:data-[count='0']:hidden"
-      href="/cart"
+      href="/checkout"
     >
       <ShoppingCart size={22} className="fill-primary-700 text-primary-700" />
     </Link>
