ba15a30926c248736c5dc7af4bb39648c31d7c7f
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-11-18 22:57:33 -0300

    Implemented checkout summary component for order finalization

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import { Input } from '@/components/Input'
 import { InputRadio } from '@/components/InputRadio'
-import { MapPin } from 'lucide-react'
+import { ProductInCart } from '@/components/ProductInCart'
+import { useCart } from '@/hooks/useCart'
+import { DollarSign, MapPin } from 'lucide-react'
 
 export default function Page() {
+  const { coffees } = useCart()
+
   return (
     <div className="container mx-auto flex gap-8 pt-24">
       <div className="w-3/5 space-y-3">
@@ -35,7 +40,7 @@ export default function Page() {
         </div>
         <div className="rounded-md bg-base-card p-10">
           <div className="mb-8 flex gap-2">
-            <MapPin className="text-primary-700" />
+            <DollarSign className="text-secondary-500" />
             <div>
               <span className="text-base-subtitle">Pagamento</span>
               <p className="text-sm text-base-text">
@@ -54,6 +59,35 @@ export default function Page() {
               Dinheiro
             </InputRadio>
           </div>
+        </div>
+      </div>
+      <div className="w-2/5 space-y-3">
+        <p className="font-title text-lg font-bold text-base-subtitle">
+          Complete seu pedido
+        </p>
+        <div className="bg-base-card p-10">
+          <div className="flex flex-col items-center overflow-auto">
+            {coffees.map((coffee) => (
+              <ProductInCart key={coffee.id} coffee={coffee} />
+            ))}
+          </div>
+          <div className="my-6 space-y-3">
+            <div className="flex items-center justify-between">
+              <p>Totoal de itens</p>
+              <span>R$ 29,70</span>
+            </div>
+            <div className="flex items-center justify-between">
+              <p>Entrega</p>
+              <span>R$ 3,50</span>
+            </div>
+            <div className="flex items-center justify-between text-xl font-bold text-base-subtitle">
+              <p>Total</p>
+              <span>R$ 33,20</span>
+            </div>
+          </div>
+          <button className="w-full rounded bg-primary-500 py-3 text-center text-sm font-bold text-white">
+            Confirmar pedido
+          </button>
         </div>
       </div>
     </div>
