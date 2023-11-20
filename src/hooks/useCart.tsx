b2e5063ba4cbd76250eeb53f1993e1bc659d6e52
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-11-19 23:35:41 -0300

    add incremmentedAmout, decrementedAmount and totalValue

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { produce } from 'immer'
 import { ReactNode, createContext, useContext, useState } from 'react'
 
 type Coffee = {
@@ -13,6 +14,9 @@ type CartContextData = {
   coffees: Coffee[]
   addCoffee: (coffee: Coffee) => void
   removeCoffee: (id: string) => void
+  incrementedAmount: (id: string) => void
+  decrementedAmount: (id: string) => void
+  totalValue: number
 }
 
 const CartContext = createContext({} as CartContextData)
@@ -20,28 +24,77 @@ const CartContext = createContext({} as CartContextData)
 export function CartContextProvider({ children }: { children: ReactNode }) {
   const [coffees, setCoffees] = useState<Coffee[]>([])
 
+  const totalValue = coffees.reduce(
+    (acc, coffee) => coffee.price * coffee.amount + acc,
+    0,
+  )
+
   function addCoffee(coffee: Coffee) {
-    setCoffees((state) => {
-      const productExists = state.findIndex((item) => item.id === coffee.id)
+    const coffeeIndex = coffees.findIndex((item) => item.id === coffee.id)
 
-      if (productExists < 0) {
-        return [...state, coffee]
-      }
-
-      state.splice(productExists, 1, coffee)
-
-      return state
-    })
+    if (coffeeIndex >= 0) {
+      setCoffees((state) => {
+        return produce(state, (draft) => {
+          draft.splice(coffeeIndex, 1, coffee)
+        })
+      })
+    } else {
+      setCoffees([...coffees, coffee])
+    }
   }
 
   function removeCoffee(coffeeId: string) {
     setCoffees((state) => {
-      return state.filter((coffee) => coffee.id !== coffeeId)
+      return produce(state, (draft) => {
+        const index = draft.findIndex((coffee) => coffee.id === coffeeId)
+        draft.splice(index, 1)
+      })
+    })
+  }
+
+  function incrementedAmount(coffeeId: string) {
+    const index = coffees.findIndex((coffee) => coffee.id === coffeeId)
+
+    if (index < 0) {
+      return
+    }
+
+    setCoffees((state) => {
+      return produce(state, (draft) => {
+        draft[index].amount++
+      })
+    })
+  }
+
+  function decrementedAmount(coffeeId: string) {
+    const index = coffees.findIndex((coffee) => coffee.id === coffeeId)
+
+    if (index < 0) {
+      return
+    }
+
+    if (coffees[index].amount === 1) {
+      return
+    }
+
+    setCoffees((state) => {
+      return produce(state, (draft) => {
+        draft[index].amount--
+      })
     })
   }
 
   return (
-    <CartContext.Provider value={{ addCoffee, removeCoffee, coffees }}>
+    <CartContext.Provider
+      value={{
+        addCoffee,
+        removeCoffee,
+        coffees,
+        incrementedAmount,
+        decrementedAmount,
+        totalValue,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
